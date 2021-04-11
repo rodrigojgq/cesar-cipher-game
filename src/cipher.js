55f248fa0b48a92fd80e71a9e0d33abdc03378dc
rodrigojgq <rodrigojgq@gmail.com>
2021-04-11 18:50:21 -0400

    Inicio de funciones de cifrado.

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,12 @@
 const cipher = {
-  // ...
+  encode(offset, message) {
+    message = message+offset;
+    return message
+  },
+  decode(offset, message){
+    message = message+offset;
+    return message
+  }
 };
 
 export default cipher;
