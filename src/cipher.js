a8c1b8d410e2664fa647d27d6f04aaa79e25a5d1
rodrigojgq <rodrigojgq@gmail.com>
2021-04-11 21:05:22 -0400

    Interacción básica de cifrado y descifrado terminada.

--- src/cipher.js
+++ src/cipher.js
@@ -1,11 +1,34 @@
 const cipher = {
   encode(offset, message) {
-    message = message+offset;
-    return message
+    if(typeof(offset) != 'number'){
+      throw new TypeError();
+    }
+    if(typeof(message) != 'string'){
+      throw new TypeError();
+    }
+    let result = '';
+    for(let i = 0; i<message.length; i++){
+      let codeLetter = (message.charCodeAt(i)%65+offset)%26+65;
+      result += String.fromCharCode(codeLetter);
+    }
+    return result;
   },
   decode(offset, message){
-    message = message+offset;
-    return message
+    if(typeof(offset) != 'number'){
+      throw new TypeError();
+    }
+    if(typeof(message) != 'string'){
+      throw new TypeError();
+    }
+    let result = '';
+    for(let i = 0; i<message.length; i++){
+      let codeLetter = message.charCodeAt(i)-(offset)%26;
+      if ((codeLetter - 65) < 0) {
+        codeLetter = 26+codeLetter;
+      }
+      result += String.fromCharCode(codeLetter);
+    }
+    return result;
   }
 };
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 import cipher from './cipher.js';
+const messageContainer = document.getElementById("message");
+const offsetContainer = document.getElementById("offset");
+const resultMessageContainer = document.getElementById("result-message");
+const swapButton = document.getElementById("swap");
+const firstTitle = document.getElementById("first-box-title");
+const secondTitle = document.getElementById("second-box-title");
+const buttonPlay = document.getElementById("button-play");
+const buttonCipher = document.getElementById("button-cipher-decipher");
+let offset = 0;
 
-console.log(cipher);
+localStorage.setItem("category", "decipher");
+
+messageContainer.addEventListener("keyup",changeMessage);
+offsetContainer.addEventListener("keyup",changeMessage);
+swapButton.addEventListener("click", changeCipherDecipher);
+
+function changeMessage(){
+    let message = messageContainer.value;
+    offset = Number(offsetContainer.value);
+    message = message.toUpperCase();
+    let resultMessage = '';
+    if(localStorage.getItem("category")==='cipher') {
+        resultMessage = cipher.encode(offset,message);
+    } else if (localStorage.getItem("category")==='decipher') {
+        resultMessage = cipher.decode(offset,message);
+    }
+    resultMessageContainer.innerHTML = resultMessage;
+}
+function changeCipherDecipher(){
+    if(localStorage.getItem("category")==='cipher') {
+        localStorage.setItem("category", "decipher");
+        firstTitle.innerHTML = "Cifrado";
+        secondTitle.innerHTML = "Descifrado";
+        buttonCipher.innerHTML = "Descifrar"
+    } else if (localStorage.getItem("category")==='decipher') {
+        localStorage.setItem("category", "cipher");
+        secondTitle.innerHTML = "Cifrado";
+        firstTitle.innerHTML = "Descifrado";
+        buttonCipher.innerHTML = "Cifrar"
+    }
+    changeMessage();
+}
